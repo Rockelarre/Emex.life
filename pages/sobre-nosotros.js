b0f1aba128b3b8f410fab0bef86cd6776b00657b
Cianware <leogrises@gmail.com>
2021-04-14 15:01:22 -0400

    dev_1

--- pages/sobre-nosotros.js
+++ pages/sobre-nosotros.js
@@ -32,6 +32,7 @@ class About extends Component {
               },
               description: "En esta página se describe a la empresa en mayor profundidad, nuestros valores y fundamentos, exresados a través de la misión, visión y quienes somos.",
               funder: "EMEX - Control de Plagas",
+              url: "http://www.facebook.com/emex.life",
               keywords: "control de plagas,sanitización,desratización,desinsectación,control de palomas,instalación de pinchos para aves,control de murciélagos,trampas ultravioleta,roedores,ratones,ratas,guarenes,pericotes,insectos,termitas,cucarachas,baratas,chinches,moscas,mosquitos,arácnidos,arañas,murciélagos,sanitización de espacios comunes,virus,coronavirus,tifus,influenza,bacterias,estreptococo,hongos,asesorías técnicas,control,prevención,educación,casas particulares,empresas manufactureras,empresas comerciales,industrias,condominios,oficinas,restaurantes,instituciones educativas,entes gubernamentales,planes de servicios,medios de pago,efectivo,transferencia bancaria,tarjeta de crédito"
             })
         }}
