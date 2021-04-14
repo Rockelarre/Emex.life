483dcec4afee1b7c433a0204391f04404970fecd
Cianware <leogrises@gmail.com>
2021-04-14 13:57:01 -0400

    dev_1

--- pages/contacto.js
+++ pages/contacto.js
@@ -16,18 +16,18 @@ class Contact extends Component {
           {{
             "@context": "http://schema.org",
             "@type": "Empresa",
-            author: {
+            "author": {
                 "@type": "Person",
-                email: "mailto:leogrises@gmail.com",
+                "email": "mailto:leogrises@gmail.com",
                 /* image: "janedoe.jpg", */
-                jobTitle: "Developer",
-                name: "Daniel Vega Caro",
-                telephone: "(+56) 9 6702 0377",
-                url: "http://www.cianware.com"
+                "jobTitle": "Developer",
+                "name": "Daniel Vega Caro",
+                "telephone": "(+56) 9 6702 0377",
+                "url": "http://www.cianware.com"
               },
-              description: "En esta página se presentan los representante de la empresa EMEX - Control de Plagas, y sus datos de contacto, teléfono, email, y también se muestra un teléfono para contactarse por WhatsApp directamente para agendar y comprar servicios.",
-              funder: "EMEX - Control de Plagas",
-              keywords: "control de plagas,sanitización,desratización,desinsectación,control de palomas,instalación de pinchos para aves,control de murciélagos,trampas ultravioleta,roedores,ratones,ratas,guarenes,pericotes,insectos,termitas,cucarachas,baratas,chinches,moscas,mosquitos,arácnidos,arañas,murciélagos,sanitización de espacios comunes,virus,coronavirus,tifus,influenza,bacterias,estreptococo,hongos,asesorías técnicas,control,prevención,educación,casas particulares,empresas manufactureras,empresas comerciales,industrias,condominios,oficinas,restaurantes,instituciones educativas,entes gubernamentales,planes de servicios,medios de pago,efectivo,transferencia bancaria,tarjeta de crédito"
+              "description": "En esta página se presentan los representante de la empresa EMEX - Control de Plagas, y sus datos de contacto, teléfono, email, y también se muestra un teléfono para contactarse por WhatsApp directamente para agendar y comprar servicios.",
+              "funder": "EMEX - Control de Plagas",
+              "keywords": "control de plagas,sanitización,desratización,desinsectación,control de palomas,instalación de pinchos para aves,control de murciélagos,trampas ultravioleta,roedores,ratones,ratas,guarenes,pericotes,insectos,termitas,cucarachas,baratas,chinches,moscas,mosquitos,arácnidos,arañas,murciélagos,sanitización de espacios comunes,virus,coronavirus,tifus,influenza,bacterias,estreptococo,hongos,asesorías técnicas,control,prevención,educación,casas particulares,empresas manufactureras,empresas comerciales,industrias,condominios,oficinas,restaurantes,instituciones educativas,entes gubernamentales,planes de servicios,medios de pago,efectivo,transferencia bancaria,tarjeta de crédito"
           }}
         </script>
                 </Head>
