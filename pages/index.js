4454c8eeddaf74587557303ed97470513fedc222
Cianware <leogrises@gmail.com>
2021-04-14 14:25:20 -0400

    dev_1

--- pages/index.js
+++ pages/index.js
@@ -20,24 +20,26 @@ class Index extends Component {
             <React.Fragment>
                 <Head>
                 <title>EMEX - Control de Plagas - Inicio</title>
-                <script type="application/ld+json">
-          {{
+                <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify({
             "@context": "http://schema.org",
             "@type": "Empresa",
-            "author": {
+            author: {
                 "@type": "Person",
-                "email": "mailto:leogrises@gmail.com",
+                email: "mailto:leogrises@gmail.com",
                 /* image: "janedoe.jpg", */
-                "jobTitle": "Developer",
-                "name": "Daniel Vega Caro",
-                "telephone": "(+56) 9 6702 0377",
-                "url": "http://www.cianware.com"
+                jobTitle: "Developer",
+                name: "Daniel Vega Caro",
+                telephone: "(+56) 9 6702 0377",
+                url: "http://www.cianware.com"
               },
-              "description": "Esta es la página de inicio de este sitio web, se describen los tipos de servicios de control de plagas, roedores, insectos, arácnidos, palomas, murciélagos, sanitización de espacios comunes, virus, bacterias, hongos, asesorías técnicas, control, prevencion, educación. Listamos una serie de lugares y negocios a los que prestamos el servicio, casas particulares, empresas manufactureras, empresas comerciales, industrias, condominios, oficinas, restaurantes, instituciones educativas, entes gubernamentales. Listamos a nuestros clientes y colaboradores. Ofrecemos precios por cada servicio y planes de servicios. Ofrecemos medios de pago, efectivo, transferencia bancaria, tarjeta de crédito. ",
-              "funder": "EMEX - Control de Plagas",
-              "keywords": "control de plagas,sanitización,desratización,desinsectación,control de palomas,instalación de pinchos para aves,control de murciélagos,trampas ultravioleta,roedores,ratones,ratas,guarenes,pericotes,insectos,termitas,cucarachas,baratas,chinches,moscas,mosquitos,arácnidos,arañas,murciélagos,sanitización de espacios comunes,virus,coronavirus,tifus,influenza,bacterias,estreptococo,hongos,asesorías técnicas,control,prevención,educación,casas particulares,empresas manufactureras,empresas comerciales,industrias,condominios,oficinas,restaurantes,instituciones educativas,entes gubernamentales,planes de servicios,medios de pago,efectivo,transferencia bancaria,tarjeta de crédito"
-          }}
-        </script>
+              description: "Esta es la página de inicio de este sitio web, se describen los tipos de servicios de control de plagas, roedores, insectos, arácnidos, palomas, murciélagos, sanitización de espacios comunes, virus, bacterias, hongos, asesorías técnicas, control, prevencion, educación. Listamos una serie de lugares y negocios a los que prestamos el servicio, casas particulares, empresas manufactureras, empresas comerciales, industrias, condominios, oficinas, restaurantes, instituciones educativas, entes gubernamentales. Listamos a nuestros clientes y colaboradores. Ofrecemos precios por cada servicio y planes de servicios. Ofrecemos medios de pago, efectivo, transferencia bancaria, tarjeta de crédito. ",
+              funder: "EMEX - Control de Plagas",
+              keywords: "control de plagas,sanitización,desratización,desinsectación,control de palomas,instalación de pinchos para aves,control de murciélagos,trampas ultravioleta,roedores,ratones,ratas,guarenes,pericotes,insectos,termitas,cucarachas,baratas,chinches,moscas,mosquitos,arácnidos,arañas,murciélagos,sanitización de espacios comunes,virus,coronavirus,tifus,influenza,bacterias,estreptococo,hongos,asesorías técnicas,control,prevención,educación,casas particulares,empresas manufactureras,empresas comerciales,industrias,condominios,oficinas,restaurantes,instituciones educativas,entes gubernamentales,planes de servicios,medios de pago,efectivo,transferencia bancaria,tarjeta de crédito"
+            })
+        }}
+   />
                 </Head>
                 <Navbar />
                 <MainBanner />
