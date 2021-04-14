16782997cca06819c9f2f57f0b0aaa059d73e806
Cianware <leogrises@gmail.com>
2021-04-13 23:56:37 -0400

    dev_1

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://cl.emex.life',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
